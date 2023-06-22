52945760993ea9a6ccc33663bd8ad25141f80bda
Aco <acothaha76@gmail.com>
2023-06-22 15:43:55 +0700

    revise python script

--- Python/usable_script/download_file_from_url.py
+++ Python/usable_script/download_file_from_url.py
@@ -1,10 +1,20 @@
 import requests
+import os
 
-def download_file(url):
+def download_file(url, path=''):
     local_filename = url.split('/')[-1]
+    
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
-        with open(local_filename, 'wb') as f:
+
+        if path == '':
+            pass
+        else:
+            os.makedirs(path, exist_ok=True) 
+            path = path + '/'
+            
+        with open(path + local_filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=4000): 
                 f.write(chunk)
+
     return local_filename
